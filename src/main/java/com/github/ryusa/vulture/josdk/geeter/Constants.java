4c08abc48cd4afc171bda6a239f90f17d1b3c029
RyuSA <kommsussertad@gmail.com>
2022-02-11 13:38:34 +0900

    一部の値を定数に固定

---
+++ src/main/java/com/github/ryusa/vulture/josdk/geeter/Constants.java
@@ -0,0 +1,8 @@
+package com.github.ryusa.vulture.josdk.geeter;
+
+public final class Constants {
+  public static final String CRDS_GROUP = "vulture.ryusa.github.com";
+  public static final String CRDS_VERSION = "v1alpha1";
+  public static final String SHORT_NAME = "gr";
+  public static final String PLURAL_NAME = "greeters";
+}
--- src/main/java/com/github/ryusa/vulture/josdk/geeter/crds/Greeter.java
+++ src/main/java/com/github/ryusa/vulture/josdk/geeter/crds/Greeter.java
@@ -1,16 +1,19 @@
 package com.github.ryusa.vulture.josdk.geeter.crds;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.ryusa.vulture.josdk.geeter.Constants;
 
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
 
-@Group("com.github.ryusa.vulture")
-@Version("v1alpha")
-@ShortNames("gr")
+@Group(Constants.CRDS_GROUP)
+@Version(Constants.CRDS_VERSION)
+@ShortNames(Constants.SHORT_NAME)
+@Plural(Constants.PLURAL_NAME)
 public class Greeter extends CustomResource<Greeter.GreeterSpec, Greeter.GreeterStatus> implements Namespaced {
 
   public static final class GreeterSpec {
